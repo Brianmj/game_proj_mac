2ce4b8327bf2ce88099d011d0dbd95f0f0c7748c
brianmj <spamthiseml@hotmail.com>
2018-08-17 23:13:06 -0400

    Initial Commit

---
+++ gameprojmac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gameprojmac
+//
+//  Created by Brian Jones on 8/17/18.
+//  Copyright © 2018 Brian Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
